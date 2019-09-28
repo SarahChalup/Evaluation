14f6c737a1fdcfb8d650713eee9d07282d53abe0
sarit <sarit@DESKTOP-KSJF0BF>
2019-09-28 02:36:36 -0400

    H

---
+++ WebApp/WebApp/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public enum Gender
+    {
+        Female,
+        male
+
+    }
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string Name { get; set; }
+        public Gender gender { get; set; }
+       
+        public DateTime Birthdate { get; set; }
+    }
+}
